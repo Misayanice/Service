2719e61087fa4c75576dcb621b41fe4260cfd033
Misayanice <a1169949235a@163.com>
2014-04-19 16:32:28 +0800

    Revert "test"
    This reverts commit c1dd2030e3cf215e6f1a8c124e395a828d0b5327.

---
+++ Duck/src/com/duck/test/Quack.java
@@ -0,0 +1,11 @@
+package com.duck.test;
+
+public class Quack implements QuackBehavior {
+
+	@Override
+	public void quack() {
+		// TODO Auto-generated method stub
+		System.out.println("Quack");
+	}
+
+}
---
+++ Duck/src/com/duck/test/QuackBehavior.java
@@ -0,0 +1,5 @@
+package com.duck.test;
+
+public interface QuackBehavior {
+	public void quack();
+}
